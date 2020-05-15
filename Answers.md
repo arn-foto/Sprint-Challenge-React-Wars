63765d1ee1da4253085d8b72b4d0fd0de51fab40
arn-foto <38259824+arn-foto@users.noreply.github.com>
2020-05-15 10:07:44 -0700

    Trying to get cards to go in a row

--- Answers.md
+++ Answers.md
@@ -2,8 +2,17 @@
 
 1. What is React JS and what problems does it solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
-1. Describe component state.
+React Js is a library which is declarative, component-based, and can be used as much or as little as needed. You can download dependencies and use them in order to better have DRY code. React also has some dependencies such as useState and useEffect that will allow you to do big things with less code.
 
-1. Describe props.
+2. Describe component state.
 
-1. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+Component state is an object that determines how that componet renders and behaves. It will allow you to create componenents that are dynamic and interactive.
+
+3. Describe props.
+
+ Props are special keywords that stands for properties that are used fro passing data from one component to another. Props are only passed uni-directionally.
+
+4. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+
+
+Side effects are things you want to do outside of the scope of the current function. You will use useEffect in order to sync effects in React.
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from '../src/components/Character';
+import styled from 'styled-components';
+
+const H = styled.h1`
+font-size: 4rem;
+  text-shadow: 3px 3px 3px red;
+  color:white;
+  font-family: 'Indie Flower', cursive;
+`;
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacters]= useState([]);
+  console.log("Characters", characters[0])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
+  useEffect(()=>{
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(response => setCharacters(response.data.results))
+    .catch(error =>console.log('AWW!', error))
+  },[])
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+   <div className='App'>
+      <H>Characters</H>
+
+      <div className='character-container'>
+
+      {characters.map(character =>(
+        <Character key={character.id} 
+        name={character.name} 
+        img={character.image}
+        gender={character.gender} 
+        status={character.status} 
+        species={character.species}/>
+      ))}   
+</div>
     </div>
   );
 }
